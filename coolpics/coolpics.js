e7a38822121627db42b8039852653e5d125f906a
winwin32 <collinbrun@gmail.com>
2025-02-01 18:35:20 -0600

    Added menu js and popout viewer

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,22 @@
 const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+const viewerPopout = document.querySelector(".viewer")
+const xButton = document.querySelector(".viewer button")
 
 menuToggle.addEventListener('click', () => {
+  const navLinks = document.querySelector("nav")
   navLinks.classList.toggle('show');
+});
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.classList.add("view-toggle")
+})
+
+document.querySelectorAll('.view-toggle').forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    viewerPopout.classList.toggle('show');
+  });
+});
+
+xButton.addEventListener('click', () => {
+  viewerPopout.classList.remove('show');
 });
---
+++ debugging/debugging.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  return radius * PI;
+}
+area = circleArea(3);
+console.log(area);
